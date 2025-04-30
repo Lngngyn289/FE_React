1ae631fc5d3f8f7a60416853d5cdfda5251c43ed
Lngngyn289 <longnguyenduc98921@gmail.com>
2025-04-30 16:25:05 +0700

    get all users

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -95,7 +95,7 @@ class Login extends Component {
                                 onChange={(event) => {this.handleOnChangePassword(event)}}
                                 />
                                 <span onClick={(event) => {this.handleShowHidePassword(event)}}>
-                                    <i class={this.state.isShowPassword? "fas fa-eye" : "fas fa-eye-slash"}></i>
+                                    <i className={this.state.isShowPassword? "fas fa-eye" : "fas fa-eye-slash"}></i>
                                 </span>
                             </div>
                         </div>
@@ -113,7 +113,7 @@ class Login extends Component {
                         </div>
                         <div className="col-12 social-login">
                             <i className="fab fa-google google"></i>
-                            <i class="fab fa-facebook-f facebook"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
                         </div>
                     </div>
                 </div>
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+        console.log('check response:', response);
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title">Manage Users with Long</div>
+                <div className="users-table mt-4 mx-4">
+                    <table id="customers">
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+                                {arrUsers && arrUsers.length > 0 ? (
+                                    arrUsers.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{item.email}</td>
+                                                <td>{item.firstName}</td>
+                                                <td>{item.lastName}</td>
+                                                <td>{item.address}</td>
+                                                <td>
+                                                    <button className="btn-edit"><i class="fas fa-pencil-alt"></i></button>
+                                                    <button className="btn-delete"><i class="fas fa-trash"></i></button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                ) : (
+                                    <tr>
+                                        <td colSpan="5">No users found</td>
+                                    </tr>
+                                )}
+                        </tbody>
+                        
+                    </table>
+                </div>
+            </div>
         );
     }
 
